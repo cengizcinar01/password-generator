c347052888458f1dcb6bfd003175b9cfdf987a01
Cengiz <154424137+cengizcinar01@users.noreply.github.com>
2024-01-18 20:29:19 +0100

    Added DOM elements selector functionality
    This allows for easier manipulation and control of elements within the user interface.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 () => {
     const $ = (selector) => document.querySelector(selector);
     const $$ = (selector) => document.querySelectorAll(selector);
+
+    const DOM = {
+        alert: $('.alert'),
+        password: $('.password'),
+        copyBtn: $('.copy-btn'),
+        charLength: $('.label-container span'),
+        charInput: $('.slider-range'),
+        numbersSwitch: $('input[name="numbers-switch"]'),
+        symbolsSwitch: $('input[name="symbols-switch"]'),
+        strengthBars: $$('.strength-meter .bars .bar'),
+        submitBtn: $('.submit-btn'),
+    };
 };
